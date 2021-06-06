cad068aaeb4e191866ff4efb1535e21f2b492c9c
diggitydingdong <mitchellinminecraft@gmail.com>
2021-06-06 14:21:50 +1000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/floodit_ai.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/floodit_ai.dir/main.cpp.obj"
+  "floodit_ai.exe"
+  "floodit_ai.exe.manifest"
+  "floodit_ai.pdb"
+  "libfloodit_ai.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/floodit_ai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
